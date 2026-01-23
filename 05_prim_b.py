0dbf24bf8a5271c3e213632435f7b99a7abbe147
hummel-stack <thummelaich@gmail.com>
2026-01-23 08:42:47 +0100

    h

--- 05_prim_b.py
+++ 05_prim_b.py
@@ -4,12 +4,21 @@ def prime_num(num):
     '''
     Berechnet Primzahl
     '''
-
-
-# Aufgabe: Erweitern sie dies Funktion mit einem Algorithmus, 
-# dass bei einer Primzahl True zurück gegeben soll.
-
-    return False
+    # Rückgabe: True wenn num Primzahl ist, sonst False
+    if num < 2:
+        return False
+    if num == 2:
+        return True
+    if num % 2 == 0:
+        return False
+    import math
+    max_div = int(math.isqrt(num))
+    i = 3
+    while i <= max_div:
+        if num % i == 0:
+            return False
+        i += 2
+    return True
 
 
 
@@ -17,12 +26,32 @@ def prime_num(num):
 max_num = 10
 prime_nums = {}
 
-for z in range(0, max_num + 1):
-    
-    if prime_num(z):
-        print("                Ich bin eine Primzahl: ", z)
-        #prime_nums[z] = prime_num(z)
-    # else:
-    #     print(z,"ist keine Primzahl")
-
-#print(prime_nums)
+if __name__ == "__main__":
+    import sys
+    # Kommandozeilen-Argument bevorzugen
+    if len(sys.argv) > 1:
+        try:
+            n = int(sys.argv[1])
+        except ValueError:
+            print("Bitte eine gültige ganze Zahl als Argument angeben.")
+            sys.exit(1)
+        print(prime_num(n))
+    else:
+        # Interaktive Eingabe (einmalig)
+        try:
+            s = input("Gib eine Zahl ein (oder ENTER, um Primzahlen bis max_num zu sehen): ").strip()
+        except EOFError:
+            s = ''
+        if s:
+            try:
+                n = int(s)
+            except ValueError:
+                print("Bitte eine gültige ganze Zahl eingeben.")
+            else:
+                print(prime_num(n))
+        else:
+            # Standardverhalten: Primzahlen bis max_num ausgeben
+            for z in range(0, max_num + 1):
+                if prime_num(z):
+                    print("                Ich bin eine Primzahl: ", z)
+            #print(prime_nums)
